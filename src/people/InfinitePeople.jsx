b57e2961c6642c24ac0d6fa75d86ac5c7518fc9f
Patrick Asafe <patrickasafe@gmail.com>
2022-02-23 23:21:09 -0300

    ADD useInfiniteQuery for people

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -10,7 +10,15 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
     "sw-people",
     ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     {
@@ -18,6 +26,27 @@ export function InfinitePeople() {
     }
   );
 
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div className="error">'Error!' {error.toString()}</div>;
+
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
